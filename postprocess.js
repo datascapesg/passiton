f471db7d90d77a3e3ba1b688a2ccaffff137e55c
LoneRifle <LoneRifle@users.noreply.github.com>
2021-05-22 19:17:51 +0800

    Stop trusting writeJSON

--- postprocess.js
+++ postprocess.js
@@ -1,9 +1,12 @@
-import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
+import { readJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
 
 // Read the downloaded_filename JSON
 const requests = await readJSON('requests.json')
 const offers = await readJSON('offers.json')
 
 // Write a new JSON file with our filtered data
-await writeJSON(Deno.env.get('POSTPROCESS_FILENAME'), { requests, offers }, { spaces: 2 })
+const jsonString = JSON.stringify({ requests, offers }, null, 2)
+
+Deno.writeTextFileSync(Deno.env.get('POSTPROCESS_FILENAME'), jsonString)
+    
 console.log('Done.')
