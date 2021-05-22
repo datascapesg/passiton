40ebf6b7df275ca49d3d90245547265d0f1a3881
LoneRifle <alwyn.tan@gmail.com>
2021-05-22 10:01:19 +0000

    Postprocess requests and offers into one file

---
+++ postprocess.js
@@ -0,0 +1,9 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
+
+// Read the downloaded_filename JSON
+const requests = await readJSON('requests.json')
+const offers = await readJSON('offers.json')
+
+// Write a new JSON file with our filtered data
+await writeJSON(Deno.env.get('POSTPROCESS_FILENAME'), { requests, offers })
+console.log('Done.')
